3c9f92296510c6a410cc5b8d47b9ba6187957008
titan2gman <volctondal@gmail.com>
2025-11-28 23:15:41 +0900

    add a new unique column for email

--- db/migrate/20140412113810_add_comment_notification_settings_to_users.rb
+++ db/migrate/20140412113810_add_comment_notification_settings_to_users.rb
@@ -4,5 +4,6 @@ class AddCommentNotificationSettingsToUsers < ActiveRecord::Migration
     add_column :users, :send_comment_web, :boolean, default: true
     add_column :users, :send_mention_email, :boolean, default: true
     add_column :users, :send_mention_web, :boolean, default: true
+    
   end
 end
